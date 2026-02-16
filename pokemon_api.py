32139ae4b90c3476e4d69fb01dcb4711722af50d
Jamie Brown <Jamie Brown>
2026-02-16 23:38:32 +0000

    foot: API function added to "pokemon_api.py" to get pokemon generation name

--- pokemon_api.py
+++ pokemon_api.py
@@ -34,3 +34,15 @@ def get_all_pokemon_info(name):
         return pokemon_data
     else:
         print(f"Failed to retrieve data {response.status_code}")
+
+def get_pokemon_gen(gen):
+    url = f"{base_url}/version-group/{gen}"
+    requests.get(url)
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return pokemon_data
+    else:
+        print(f"Failed to retrieve data {response.status_code}")
+
