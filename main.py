cd895b93adf385ff57d1ecbe1a281d55e1f66a3e
Jamie Brown <Jamie Brown>
2026-02-17 00:35:24 +0000

    foot: Added in data validation for pokemon name. Allowing user to retry if entered incorrectly

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ from interface import get_user_input
 from models import Pokemon
 
 def main():
-    #Getting input from user around pokemon and generation of pokemon
-    pokemon, gen = get_user_input()
-    #Retrieving Pokemon Info
-    pokemon_info = get_pokemon_info(pokemon)
-    #Retrieving evolution data for pokemon
-    evo_data = get_pokemon_evo(pokemon_info["id"])
+    pokemon_info = []
+    while not pokemon_info:
+        #Getting input from user around pokemon and generation of pokemon
+        pokemon, gen = get_user_input()
+        #Retrieving Pokemon Info
+        pokemon_info = get_pokemon_info(pokemon)
+        if pokemon_info:
+            #Retrieving evolution data for pokemon
+            evo_data = get_pokemon_evo(pokemon_info["id"])
+        else:
+            print("Invalid Pokemon")
     #Building pokemon object
     my_pokemon = Pokemon(pokemon_info, evo_data)
     #Displaying Pokemon
