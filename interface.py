dd3b428ba0df237368d340069fea28ebed260845
Jamie Brown <Jamie Brown>
2026-02-20 16:54:53 +0000

    foot: Fixed issue with missing boolean in get_gen_input function, and changed while loop operator to True, and added a break

--- interface.py
+++ interface.py
@@ -2,7 +2,8 @@
 
 def get_gen_input(all_gen):
     i = 1
-    while is_running:
+    
+    while True:
         #Looping round all items in dictionary in order to print out all available generations for user to pick
         for g in all_gen:
             print(f"{i}: {g}")
@@ -15,15 +16,15 @@ def get_gen_input(all_gen):
         choice-=1
         #Validating user input
         if choice > -1 and choice < len(all_gen):
-            is_running = False
             #Assigning the choice to the gen variable to pass back
             gen = all_gen[choice]
+            break
             
         else:
             print("Please enter a valid input ranging from 1-19")
 
     #returning chosen pokemona and pokemon gen
-    return pokemon, gen
+    return gen
 
 def get_pokemon_input():
     #Prompting user for pokemon input
